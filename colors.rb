e0816ff0b2e789cba393b6cf8afd5513cd230972
Courteney Ervin <courteney.ervin@gmail.com>
2015-11-17 16:42:24 -0500

    Add color translations

--- colors.rb
+++ colors.rb
@@ -1,3 +1,16 @@
 COLORS = {
-  blue: "\033[0;34m"
+  blue: "\033[0;34m",
+  black: "\033[0;30m",
+  green: "\033[0;32m",
+  cyan: "\033[0;36m",
+  red: "\033[0;31m",
+  purple: "\033[0;35m",
+  brown: "\033[0;33m",
+  yellow: "\033[1;33m",
+  light_gray: "\033[0;37m",
+  light_blue: "\033[1;34m",
+  light_green: "\033[1;32m",
+  light_cyan: "\033[1;36m",
+  light_red: "\033[1;31m",
+  light_purple: "\033[1;35m"
 }
